e11dc497172f6d019fba9874771a41d844652edf
Aapeli Poranen <aapeli.poranen@gmail.com>
2025-10-01 17:39:55 +0300

    remove dayjs. fix uptime

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,16 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
-const dayjs = require('dayjs');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("pekkahereillä")
     .setDescription("kauanko pekka on ollut hereillä"),
     async execute(interaction) {
-        const lastLogin = dayjs(interaction.client.readyAt);
-        const now = dayjs();
-        const days = now.diff(lastLogin, "day");
-        const hours = now.diff(lastLogin, "hour");
-        const minutes = now.diff(lastLogin, "minute");
+        const uptime = interaction.client.uptime;
+        // get total seconds
+        let delta = uptime / 1000;
+
+        // calculate (and subtract) whole days
+        const days = Math.floor(delta / 86400);
+        delta -= days * 86400;
+
+        // calculate (and subtract) whole hours
+        const hours = Math.floor(delta / 3600) % 24;
+        delta -= hours * 3600;
+
+        // calculate (and subtract) whole minutes
+        const minutes = Math.floor(delta / 60) % 60;
+        delta -= minutes * 60;
+        
         await interaction.reply(`Olen ollut hereillä ${days} päivää, ${hours} tuntia ja ${minutes} minuuttia.`);
     }
 }

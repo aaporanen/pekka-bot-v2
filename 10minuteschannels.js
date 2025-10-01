1e1f61398ee906d4003c9573625bab2b5593a744
Aapeli Poranen <aapeli.poranen@gmail.com>
2025-10-01 17:51:13 +0300

    add default schedule channel

--- 10minuteschannels.js
+++ 10minuteschannels.js
@@ -1,3 +1,7 @@
 const tenMinutesLeftChannels = [];
 
+if (process.env.DEFAULT_SCHEDULE_CHANNEL) {
+    tenMinutesLeftChannels.push(process.env.DEFAULT_SCHEDULE_CHANNEL);
+}
+
 module.exports = tenMinutesLeftChannels;
--- 420channels.js
+++ 420channels.js
@@ -1,3 +1,7 @@
 const smokeWeedChannels = [];
 
+if (process.env.DEFAULT_SCHEDULE_CHANNEL) {
+    smokeWeedChannels.push(process.env.DEFAULT_SCHEDULE_CHANNEL);
+}
+
 module.exports = smokeWeedChannels;

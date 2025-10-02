7854345ec054cef2ed4b204dfca751e18417aeb0
Aapeli Poranen <aapeli.poranen@gmail.com>
2025-10-02 15:29:10 +0300

    edit johtaja command response msg

--- commands/iliveagain.js
+++ commands/iliveagain.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription("johtaja"),
     async execute(interaction) {
         const file = new AttachmentBuilder('./resources/2841611.jpg');
-        const embed = new EmbedBuilder().setTitle("I live... again.").setImage('attachment://2841611.jpg')
+        const embed = new EmbedBuilder().setTitle("Tervehdys.").setImage('attachment://2841611.jpg')
         await interaction.reply({ embeds: [embed], files: [file] });
     }
 }

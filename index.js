b525d63ed7ba93baa184bd5a708f6f2928b8dd33
Aapeli Poranen <aapeli.poranen@gmail.com>
2025-10-02 23:59:27 +0300

    testing..

--- index.js
+++ index.js
@@ -113,15 +113,15 @@ const sendChristmas = schedule.scheduleJob(ruleChristmas, async () => {
 });
 
 const ruleFridayGambina = new schedule.RecurrenceRule();
-ruleFridayGambina.hour = 23;
-ruleFridayGambina.minute = 58;
+ruleFridayGambina.hour = 0;
+ruleFridayGambina.minute = 2;
 ruleFridayGambina.tz = 'Europe/Helsinki';
 
 const fridayRaffle = schedule.scheduleJob(ruleFridayGambina, async () => {
 	fridayRaffleChannels.forEach(id => {
         const channel = client.channels.cache.get(id);
 		console.log(channel);
-		const users = channel.members.list();
+		const users = channel.guild.members.list();
 		const winner = users[getRandomInteger(0, users.length)];
 
         const file = new AttachmentBuilder(`./images/gambina.png`);
